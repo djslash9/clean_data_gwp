6b85b865d18444aa0013cc9797844da45a2e5ee6
DJ Slash <djslash9@users.noreply.github.com>
2022-04-30 11:04:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -94,6 +94,13 @@ def main():
             file_name='vhcl_dup.csv',
             mime='text/csv',
             ) 
-    
+        
+    hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 if __name__ == '__main__':
     main()
